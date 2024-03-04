adbb77714e375508c7b9ffa6e1e660c345c52665
Grid <101964953+ZethT@users.noreply.github.com>
2024-03-03 18:54:09 -0800

    new speed just dropped

--- Echoes of Fear/Elena.gd
+++ Echoes of Fear/Elena.gd
@@ -4,7 +4,7 @@ extends CharacterBody3D
 var held_item
 
 
-const SPEED = 5.0
+const SPEED = 4.0
 const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
