6fa199260464c0107a31ff4dd05520b856017a10
antmanantman33 <antrbolinger@gmail.com>
2024-03-21 20:56:35 -0700

    start button sends player to game instead of testing stage

--- Echoes of Fear/title_screen.gd
+++ Echoes of Fear/title_screen.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_start_pressed():
-	get_tree().change_scene_to_file("res://Testing/testing_stage.tscn")
+	get_tree().change_scene_to_file("res://Game.tscn")
 
 
 func _on_options_pressed():
