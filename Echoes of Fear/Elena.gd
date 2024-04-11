093c44f62a4ecbcf4e566d71dba2b02c0c60ef83
anxonlee <125641879+anxonlee@users.noreply.github.com>
2024-04-11 14:11:54 -0700

    footsteps

--- Echoes of Fear/Elena.gd
+++ Echoes of Fear/Elena.gd
@@ -9,6 +9,7 @@ var speed = 5.0
 const JUMP_VELOCITY = 4.5
 
 @onready var progress = $UI/Reticle/Progress
+@onready var concreteFootsteps = $concreteFootsteps
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -31,12 +32,14 @@ func _physics_process(delta):
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
+		concreteFootsteps.pitch_scale = randf_range(0.8,1.2)
+		if not concreteFootsteps.playing:
+			concreteFootsteps.play()
 		velocity.x = direction.x * speed
 		velocity.z = direction.z * speed
 	else:
 		velocity.x = 0
 		velocity.z = 0
-
 	if Input.is_action_pressed("interact"):
 		check_interaction(delta)
 	else:
---
+++ Echoes of Fear/NPCs/Ghost.gd
@@ -0,0 +1,39 @@
+extends CharacterBody3D
+
+# Navigation agent node handles pathfinding for Ghost
+@onready var nav_agent = $NavigationAgent3D
+@onready var timer = $follow
+@onready var player = get_node("/root/Game/Elena")
+@onready var catStanding = $catStanding
+var speed = 2.0
+const JUMP_VELOCITY = 4.5
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+
+func _physics_process(delta):
+	var current_location = global_transform.origin
+	var next_location = nav_agent.get_next_path_position()
+	var new_velocity = (next_location - current_location).normalized() * speed
+
+	velocity = velocity.move_toward(new_velocity, .25)
+	look_at(next_location)
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	
+	move_and_slide()
+
+
+# Set the destination for the navigation agent
+func update_target_location(target_location):
+	nav_agent.target_position = target_location
+
+func _on_follow_timeout():
+	var rng = randf()
+	if rng < 0.1:
+		update_target_location(player.global_position)
+	elif rng < 0.3:
+		update_target_location(Vector3(randf_range(-15, 10), 0, randf_range(-10, 30)))
